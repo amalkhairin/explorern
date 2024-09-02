a9487f32dc2b844402a7e4f6512dba8c7a71cd85
amalkhairin <amallanch45@gmail.com>
2024-09-02 09:49:37 +0700

    init project

--- App.js
+++ App.js
@@ -1,31 +1,78 @@
+import * as SplashScreen from "expo-splash-screen";
 import { config } from "@tamagui/config/v3";
 import { useFonts } from "expo-font";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, RefreshControl, Text } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 import { createTamagui, TamaguiProvider } from "tamagui";
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+
+SplashScreen.preventAutoHideAsync();
 
 const tamaguiConfig = createTamagui(config);
 
 const App = () => {
   // @ts-ignore
-  const [loaded] = useFonts({
+  const [fontsLoaded, fontsError] = useFonts({
     // @ts-ignore
     Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
     // @ts-ignore
     InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+    // "StyleScript-Regular": require("./assets/fonts/ttf/StyleScript-Regular.ttf"),
   });
 
-  if (!loaded) {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const onLayoutRootView = async () => {
+    if (fontsLoaded || fontsError) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  if (!fontsLoaded) {
     return null;
   }
 
+  const ContentComponent = () => {
+    return (
+      <>
+      <Text>Hello World</Text>
+      </>
+    );
+  };
+
+  const onRefresh = () => {
+    console.log("App: on refresh")
+    setIsRefreshing(true)
+    setTimeout(() => {
+      setIsRefreshing(false)
+    }, 2000)
+  }
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <TamaguiProvider config={tamaguiConfig}>
-        <Text>Hello World</Text>
-      </TamaguiProvider>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <TamaguiProvider config={tamaguiConfig}>
+          <StatusBar style="light" backgroundColor="black" />
+          <FlatList 
+            data={[{}]}
+            renderItem={ContentComponent}
+            contentContainerStyle={{
+              justifyContent: "center",
+              alignItems: "center",
+              flex: 1,
+              backgroundColor:"white"
+            }}
+            onLayout={onLayoutRootView}
+            refreshControl={
+              <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
+            }
+          />
+          <Text>Hello World</Text>
+        </TamaguiProvider>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
